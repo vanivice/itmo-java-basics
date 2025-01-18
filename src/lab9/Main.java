2e8b0609100401d790b41a88d0e500c2061783fb
vanivice <ltd.vermut@mail.ru>
2025-01-18 12:43:36 +0800

    completed lab9

---
+++ src/lab9/Main.java
@@ -0,0 +1,9 @@
+package lab9;
+
+public class Main {
+    public static void main(String[] args) {
+        TheNinthLabWork.resRemoveDuplicate();
+        TheNinthLabWork.requiredMethod();
+        TheNinthLabWork.statByName();
+    }
+}
