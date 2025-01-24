35983db2b9f795bbcedb109c3c0e39f329d6a2ec
vanivice <ltd.vermut@mail.ru>
2025-01-24 21:43:12 +0800

    correction

--- src/lab10/Main.java
+++ src/lab10/Main.java
@@ -1,11 +1,9 @@
 package lab10;
 
-import java.io.IOException;
-
 import static lab10.TheTenLabWork.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         readTheFile("src/lab10/fish-text1.txt");
 
